811d8dab945d19a51aab7d5156af92c72d30069e
qtian13 <qiushuang.tian@gmail.com>
2021-06-23 18:53:49 -0700

    write pseudo code of functions in js file

--- script.js
+++ script.js
@@ -10,5 +10,26 @@ function writePassword() {
 
 }
 
+// Generate password with specified criteria
+function generatePassword() {
+// specify the length of password
+  var passwordLength = speicfyTheLength();
+// specify the possible characters can be used
+  var charactersSatisfied = generateCharactersSatisfied();
+// generate the password of certain length with charactersSatisfied and return it
+  return "123253";
+}
+
+// specify the length of password
+function speicfyTheLength() {
+  return 5;
+}
+
+// specify the possible characters can be used
+function generateCharactersSatisfied() {
+  return "abcdefg";
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
