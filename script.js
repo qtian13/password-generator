8a4b9e941cdbe0004b0d318cf115617324742c54
qtian13 <qiushuang.tian@gmail.com>
2021-06-23 19:35:36 -0700

    create an chars obj, the type selected can be concatenated to chars for generating password

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var characters = {
+  upperCase: {
+    select: false,
+    chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  },
+
+  lowerCase: {
+    select: false,
+    chars: "abcdefghijklmnopqrstuvwxyz",
+  },
+
+  number: {
+    select: false,
+    chars: "1234567890",
+  },
+
+  specialCharacter: {
+    select: false,
+    chars: "!@#$%^&*()[]{}-=_+`~\/\\,.\'\":;|?",
+  },
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -17,7 +39,8 @@ function generatePassword() {
 // specify the length of password
   password.length = speicfyTheLength();
 // specify the possible characters can be used
-  // charactersSatisfied = generateCharactersSatisfied();
+  charactersSatisfied = generateCharactersSatisfied();
+  console.log("charactersSatisfied" + charactersSatisfied);
 // generate the password of certain length with charactersSatisfied and return it
   for (var i = 0; i < password.length; i++) {
     password[i] = "a";
@@ -28,16 +51,25 @@ function generatePassword() {
 // specify the length of password
 function speicfyTheLength() {
   var length = prompt("enter a number to specify the length of the password")
+  length = parseFloat(length);
   if (!Number.isInteger(length) || length < 8 || length > 128) {
     alert("Please enter an integer between 8 to 128!");
     return speicfyTheLength();
+  } else {
+    return length;
   }
-  return length;
 }
 
 // specify the possible characters can be used
 function generateCharactersSatisfied() {
-  return "abcdefg";
+  var charactersSatisfied = "";
+  //iterate the objects in characters, concatenate the string if .selected is true
+  for (var charType in characters) {
+    if (characters[charType].select) {
+      charactersSatisfied = charactersSatisfied.concat(characters[charType].chars);
+    }
+  }
+  return charactersSatisfied;
 }
 
 
