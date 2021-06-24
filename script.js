0e57df4a565d63d9288457e2673099d209c90270
qtian13 <qiushuang.tian@gmail.com>
2021-06-24 09:49:48 -0700

    refactor the code   use function to void repetative code

--- script.js
+++ script.js
@@ -39,76 +39,68 @@ copyBtn.addEventListener("click", copyPassword);
 moreBtn.addEventListener("click", generateMorePassword);
 resetBtn.addEventListener("click", resetCharacters);
 
+// Generate a customized password
 // Write password to the #password input
 function writePassword() {
+  reset = false;
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
+// Copy generated password to clipboard
 function copyPassword() {
   if (reset) {
-    alert("Please generate a password first")
+    alert("Please generate a password first");
   } else {
     var copyText = document.querySelector("#password");
 
-    /* Select the text field */
+    //Select the text field
     copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-    /* Copy the text inside the text field */
+    copyText.setSelectionRange(0, 99999); 
+    //Copy the text inside the text field
     document.execCommand("copy");
   }
 }
 
+// Generate another password with previous criteria
 function generateMorePassword() {
   if (reset) {
     alert("Please generate a password first")
   } else {
-    var password = [];
     var passwordText = document.querySelector("#password");
-    password.length = passwordLength;
-
-    for (var i = 0; i < password.length; i++) {
-      password[i] = randomizeCharacter(characterSet);
-    }
-    passwordText.value = password.join('');
+    passwordText.value = randomizeCharacters(passwordLength, characterSet).join('');
   }
 }
 
+// Clear previous criteria and clear the text in #password
 function resetCharacters() {
   var passwordText = document.querySelector("#password");
   passwordText.value = "";
-  reset = true;
   passwordLength = 0;
   characterSet = "";
   resetCharType();
-
+  reset = true;
 }
-
 
 // Generate password with specified criteria
 function generatePassword() {
-  var password = [];
   // specify the length of password
-  reset = false;
-  password.length = setPasswordLength();
-  passwordLength = password.length;
-  // specify the possible characters can be used
+  passwordLength = setPasswordLength();
+  // specify the character set to choose from
   characterSet = generateCharacterSet();
 
-  // generate the password of certain length with charactersSatisfied and return it
-  for (var i = 0; i < password.length; i++) {
-    password[i] = randomizeCharacter(characterSet);
-  }
-  return password.join('');
+  // generate a character array of custermized length with custermized characterSed
+  // change the character array to a string
+  return randomizeCharacters(passwordLength, characterSet).join('');
 }
 
 // specify the length of password
 function setPasswordLength() {
   var length = prompt("enter a number to specify the length of the password");
   length = parseFloat(length);
+  // users must enter a valid integer before the next step
   if (!Number.isInteger(length) || length < 8 || length > 128) {
     alert("Please enter an integer between 8 to 128!");
     return setPasswordLength();
@@ -121,8 +113,10 @@ function setPasswordLength() {
 function generateCharacterSet() {
   characterSet = "";
 
+  // decide which small character set is selected
   custermizeCharType();
-//iterate the objects in characters, concatenate the string if .select is true
+
+  //concatenate selected small character set to final set
   for (var charType in characters) {
     if (characters[charType].select) {
       characterSet = characterSet.concat(characters[charType].chars);
@@ -133,26 +127,37 @@ function generateCharacterSet() {
 
 // custermize character type with confirm()
 function custermizeCharType() {
+  // customize character type
   for (var charType in characters) {
     characters[charType].select = confirm("Do you want " + characters[charType].phrase + " in your password?");
   }
 
+  // users have to choose at least one character type to generate a password
   if (!characters.upperCase.select && !characters.lowerCase.select && !characters.number.select && !characters.specialCharacter.select) {
     alert("Please choose at least one type!");
     custermizeCharType();
   }
 }
 
+// clear the record of users' character type preference
 function resetCharType() {
   for (var charType in characters) {
     characters[charType].select = false;
   }
 }
 
-// randomize a character from a given string or char array
-function randomizeCharacter(characters) {
-  var index = Math.floor(Math.random() * characters.length);
-  return characters[index];
+// return a randomized character array of given length from a given string or a char array
+function randomizeCharacters(length, characters) {
+  var chars = [];
+  for (var i = 0; i < length; i++) {
+    chars.push(characters[randomizeNumber(characters.length)]);
+  }
+  return chars;
+}
+
+// generate a random positive integer less than number
+function randomizeNumber(number) {
+  return Math.floor(Math.random() * number);
 }
 
 
