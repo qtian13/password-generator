d9be57fd0fcf8c473cc3506e85f5c352aaac500c
qtian13 <qiushuang.tian@gmail.com>
2021-06-23 21:12:00 -0700

    add property to object and remove repetative code using for loop

--- script.js
+++ script.js
@@ -5,23 +5,32 @@ var characters = {
   upperCase: {
     select: false,
     chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    phrase: "upper case alphabets",
   },
 
   lowerCase: {
     select: false,
     chars: "abcdefghijklmnopqrstuvwxyz",
+    phrase: "lower case alphabets",
   },
 
   number: {
     select: false,
     chars: "1234567890",
+    phrase: "numbers",
   },
 
   specialCharacter: {
     select: false,
     chars: "!@#$%^&*()[]{}-=_+`~\/\\,.\'\":;|?",
+    phrase: "special characters",
   },
 }
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -34,61 +43,60 @@ function writePassword() {
 // Generate password with specified criteria
 function generatePassword() {
   var password = [];
-  var charactersSatisfied;
+  var characteSet;
 
-// specify the length of password
-  password.length = speicfyTheLength();
-// specify the possible characters can be used
-  charactersSatisfied = generateCharactersSatisfied();
-// generate the password of certain length with charactersSatisfied and return it
+  // specify the length of password
+  password.length = setPasswordLength();
+  // specify the possible characters can be used
+  characteSet = generateCharacterSet();
+  // generate the password of certain length with charactersSatisfied and return it
   for (var i = 0; i < password.length; i++) {
-    password[i] = randomizeCharacter(charactersSatisfied);
+    password[i] = randomizeCharacter(characteSet);
   }
   return password.join('');
 }
 
 // specify the length of password
-function speicfyTheLength() {
+function setPasswordLength() {
   var length = prompt("enter a number to specify the length of the password");
-
   length = parseFloat(length);
   if (!Number.isInteger(length) || length < 8 || length > 128) {
     alert("Please enter an integer between 8 to 128!");
-    return speicfyTheLength();
+    return setPasswordLength();
   } else {
     return length;
   }
 }
 
 // specify the possible characters can be used
-function generateCharactersSatisfied() {
-  var charactersSatisfied = "";
+function generateCharacterSet() {
+  var characterSet = "";
 
-  custermizeCharTypeSelect();
-//iterate the objects in characters, concatenate the string if .selected is true
+  custermizeCharType();
+//iterate the objects in characters, concatenate the string if .select is true
   for (var charType in characters) {
     if (characters[charType].select) {
-      charactersSatisfied = charactersSatisfied.concat(characters[charType].chars);
+      characterSet = characterSet.concat(characters[charType].chars);
     }
   }
 //reset the choices for privacy protect
-  resetCharTypeSelect();
-  return charactersSatisfied;
+  resetCharType();
+  return characterSet;
 }
 
 // custermize character type with confirm()
-function custermizeCharTypeSelect() {
-  characters.upperCase.select = confirm("Do you want upper case alphabets in your password?");
-  characters.lowerCase.select = confirm("Do you want lower case alphabets in your password?");
-  characters.number.select = confirm("Do you want numbers in your password?");
-  characters.specialCharacter.select = confirm("Do you want special characters in your password?");
+function custermizeCharType() {
+  for (var charType in characters) {
+    characters[charType].select = confirm("Do you want" + characters[charType].phrase + "in your password?");
+  }
+
   if (!characters.upperCase.select && !characters.lowerCase.select && !characters.number.select && !characters.specialCharacter.select) {
     alert("Please choose at least one type!");
-    custermizeCharTypeSelect();
+    custermizeCharType();
   }
 }
 
-function resetCharTypeSelect() {
+function resetCharType() {
   for (var charType in characters) {
     characters[charType].select = false;
   }
@@ -101,5 +109,3 @@ function randomizeCharacter(characters) {
 }
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
